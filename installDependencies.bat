c95860f3de313ddfb0131df55e535dbf30e343b8
theCYBRIX <102550532+theCYBRIX@users.noreply.github.com>
2026-03-29 12:33:57 -0500

    feat: improved installDependencies.bat

--- installDependencies.bat
+++ installDependencies.bat
@@ -1,10 +1,37 @@
 @echo off
 
-echo Installing Dependencies...
-pip install websockets==9.1 -t ./lib
+set /p "version=Enter the desired websockets version (Eg: 9.1), or press Enter: "
 
-@echo Creating __init__.py
+if not exist lib (
+    @echo Creating lib folder...
+    mkdir lib 2>nul
+
+    if errorlevel 1 (
+        @echo ERROR! Failed to create lib folder. You may need to run the script as administrator.
+        pause
+        exit /b 1
+    )
+)
+
+echo Installing Dependencies...
+if "%version%"=="" (
+    echo Installing websockets ^(latest^)...
+    pip install websockets -t ./lib
+) else (
+    echo Installing websockets==%version%...
+    pip install websockets==%version% -t ./lib
+)
+
 cd lib
+
+@echo Creating __init__.py...
 type nul > __init__.py
 
+if errorlevel 1 (
+    @echo ERROR! Failed to create __init__.py. You may need to run the script as administrator.
+    pause
+    exit /b 1
+)
+
 @echo Finished!
+pause
